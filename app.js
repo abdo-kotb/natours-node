0601a7014c055fefa17078d5fee06a25ba93259b
abdo-kotb <86558336+abdo-kotb@users.noreply.github.com>
2022-08-23 02:30:12 +0400

    install cors

--- app.js
+++ app.js
@@ -8,6 +8,7 @@ const xss = require('xss-clean');
 const hpp = require('hpp');
 const cookieParser = require('cookie-parser');
 const compression = require('compression');
+const cors = require('cors');
 
 const tourRouter = require('./routes/tourRoutes');
 const userRouter = require('./routes/userRoutes');
@@ -26,6 +27,9 @@ app.set('views', path.join(__dirname, 'views'));
 
 ///////////////////////////
 // middlewares
+app.use(cors());
+app.options('*', cors());
+
 // serving static files
 app.use(express.static(path.join(__dirname, 'public')));
 
